4560cce331779ee7e3a0c64eff626c6dd7535556
Brian Hart <brian.hart@xylogix.net>
2021-04-09 13:27:51 -0400

    Create GeneratorBase

---
+++ xyLOGIX.Generators/GeneratorBase.cs
@@ -0,0 +1,41 @@
+﻿using xyLOGIX.Generators.Constants;
+using xyLOGIX.Generators.Interfaces;
+
+namespace xyLOGIX.Generators
+{
+    /// <summary>
+    /// Defines the events, methods, and properties that can be used by all
+    /// concrete <c>Generator</c> object implementations.
+    /// </summary>
+    /// <typeparam name="TInput">
+    /// Name of the type of the input value.
+    /// </typeparam>
+    /// <typeparam name="TOutput">
+    /// Name of the type of the output value.
+    /// </typeparam>
+    public abstract class
+        GeneratorBase<TInput, TOutput> : IGenerator<TInput, TOutput>
+    {
+        /// <summary>
+        /// A <see cref="T:MFR.Objects.Generators.Constants.GeneratorType" />
+        /// value that specifies which type of generator this object is.
+        /// </summary>
+        public abstract GeneratorType GeneratorType
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Transforms the input <paramref name="value" /> into the output value.
+        /// </summary>
+        /// <param name="value">
+        /// Reference to an instance of <typeparamref name="TInput" /> that is
+        /// the value to be transformed.
+        /// </param>
+        /// <returns>
+        /// A <typeparamref name="TOutput" /> whose value is the result of the
+        /// transformation.
+        /// </returns>
+        public abstract TOutput Generate(TInput value);
+    }
+}
