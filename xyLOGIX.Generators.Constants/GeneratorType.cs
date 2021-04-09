8eb96777cc8d3a627ed196325d7f8c17a3e15423
Brian Hart <brian.hart@xylogix.net>
2021-04-09 13:21:35 -0400

    Create GeneratorType.cs

---
+++ xyLOGIX.Generators.Constants/GeneratorType.cs
@@ -0,0 +1,18 @@
+﻿namespace xyLOGIX.Generators.Constants
+{
+    /// <summary>
+    /// Values that express the type of generator to be provided.
+    /// </summary>
+    public enum GeneratorType
+    {
+        /// <summary>
+        /// The generator provides regular expressions.
+        /// </summary>
+        RegularExpression,
+
+        /// <summary>
+        /// Unknown generator type.
+        /// </summary>
+        Unknown = -1
+    }
+}
